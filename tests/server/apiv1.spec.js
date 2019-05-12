aa3dfadbecc7b10dc36928bf198111b5cbf41143
Caleb Chadderton <calebchadz@gmail.com>
2019-05-12 11:01:58 +0000

    Fix Gate

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -29,7 +29,7 @@
 
   describe('Get Weather', function() {
 
-    it('with without city name', function() {
+    it('with without zip code', function() {
       reqMock = {
         query: {
 
@@ -41,7 +41,7 @@
       assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
     });
 
-    it('with valid city name and error from request call', function() {
+    it('with valid zip code and error from request call', function() {
       reqMock = {
         query: {
           city: 'hamilton'
@@ -60,7 +60,7 @@
       assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
     });
 
-    it('with incomplete city name', function() {
+    it('with incomplete zip code', function() {
       reqMock = {
         query: {
           city: 'hamilton'
@@ -79,7 +79,7 @@
       assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
     });
 
-    it('with valid city name', function() {
+    it('with valid zip code', function() {
       reqMock = {
         query: {
           city: 'hamilton'
@@ -116,7 +116,7 @@
   
   describe('Get Weather 2', function() {
 
-    it('with without city name', function() {
+    it('with without zip code', function() {
       reqMock = {
         query: {
 
