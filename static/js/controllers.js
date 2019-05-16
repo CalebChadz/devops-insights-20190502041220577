36b1749df2a4c25934beada603ca8ac1ee6000f8
Chadderton, Caleb Matene Saul <calebchadz@gmail.com>
2019-05-16 13:28:49 +1200

    Functional Maps

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,14 +1,32 @@
 var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
+var locations = [{ lat: -40.6187416, lng: 171.7195556 },
+                 { lat: 0, lng: 0 },
+                 { lat: 0, lng: 0 },
+                 { lat: 0, lng: 0 },
+                 { lat: 0, lng: 0 }];
+
 
 
 function initMap1() {
     // The location of Uluru
-    var uluru = { lat: -40.6187416, lng: 171.7195556 };
+    var uluru = locations[0];
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: uluru });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: uluru, map: map });
+        document.getElementById('map'), { zoom: 5, center: uluru });
+    //create some lables for the markers
+    var labels = 'ABCDE';
+    // Add some markers to them map
+    var markers = locations.map(function (location, i) {
+        if (location.lat != 0) {
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length]
+            });
+        }
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
 
 
@@ -48,23 +66,31 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
             }).then( function(response) {
                 if(which === 1) {
                     $scope.zip1Weather = response.data.weather;
+                    locations[which] = { lat: response.data.lattutude, lng: response.data.longitude };
                 } else if(which === 2) {
                     $scope.zip2Weather = response.data.weather;
+                    locations[which] = { lat: response.data.lattutude, lng: response.data.longitude };
                 } else if(which === 3) {
                     $scope.zip3Weather = response.data.weather;
+                    locations[which] = { lat: response.data.lattutude, lng: response.data.longitude };
                 } else if(which === 4) {
                     $scope.zip4Weather = response.data.weather;
+                    locations[which] = { lat: response.data.lattutude, lng: response.data.longitude };
                 } 
             });
         } else {
             if(which === 1) {
-                    $scope.zip1Weather = "";
+                $scope.zip1Weather = "";
+                locations[which] = { lat: 0, lng: 0 };
                 } else if(which === 2) {
-                    $scope.zip2Weather = "";
+                $scope.zip2Weather = "";
+                locations[which] = { lat: 0, lng: 0 };
                 } else if(which === 3) {
-                    $scope.zip3Weather = "";
+                $scope.zip3Weather = "";
+                locations[which] = { lat: 0, lng: 0 };
                 } else if(which === 4) {
-                    $scope.zip4Weather = "";
+                $scope.zip4Weather = "";
+                locations[which] = { lat: 0, lng: 0 };
                 } 
         }
     };
