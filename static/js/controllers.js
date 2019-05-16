7b6b67e12bcd3f3d6061a33cf5f6ecd0ac8d3a78
Chadderton, Caleb Matene Saul <calebchadz@gmail.com>
2019-05-17 09:55:49 +1200

    seim

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -114,7 +114,7 @@ function initMap1() {
 
         markerCluster = new MarkerClusterer(map, markers,
             { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-    }
+    };
 
     updateMarkers();
 
